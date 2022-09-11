c5d0ca6ac21cc7e186dbaae3fc6df12e27faf9a0
huffmanks <huffmanks2@gmail.com>
2022-09-11 17:38:13 -0400

    faq mobile responsive

--- projects/design/faq-accordion/app.js
+++ projects/design/faq-accordion/app.js
@@ -1,11 +1,15 @@
-const panels = document.querySelectorAll('.accordion-panel')
-panels.forEach((panel) =>
-    panel.addEventListener('click', () => {
-        if (panel.classList.contains('open')) {
-            panel.classList.remove('open')
+const panels = [...document.querySelectorAll('.accordion-panel')]
+console.log('🚀 ~ file: app.js ~ line 2 ~ panels', panels)
+
+panels.map((panel) => {
+    const panelHeader = panel.querySelector('.accordion-header')
+    console.log('🚀 ~ file: app.js ~ line 7 ~ panels.map ~ panelHeader', panelHeader)
+    panelHeader.addEventListener('click', () => {
+        if (panel.classList.contains('active')) {
+            panel.classList.remove('active')
         } else {
-            panels.forEach((otherPanels) => otherPanels.classList.remove('open'))
-            panel.classList.add('open')
+            panels.forEach((otherPanels) => otherPanels.classList.remove('active'))
+            panel.classList.add('active')
         }
     })
-)
+})
